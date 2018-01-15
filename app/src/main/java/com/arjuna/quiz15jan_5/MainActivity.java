065b385fb5b070564d0d8c33f8d2c207712a9ada
Arjuna Pratama <32661531+ArjunaPratama@users.noreply.github.com>
2018-01-15 17:09:34 +0700

    Initial commit

---
+++ app/src/main/java/com/arjuna/quiz15jan_5/MainActivity.java
@@ -0,0 +1,49 @@
+package com.arjuna.quiz15jan_5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etangka;
+    TextView txthasil;
+    Button btnhasil;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etangka = (EditText) findViewById(R.id.etangka);
+        btnhasil = (Button) findViewById(R.id.btnhasil);
+        txthasil = (TextView) findViewById(R.id.txthasil);
+
+        btnhasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String nangka = etangka.getText().toString();
+
+                int aAngka = Integer.parseInt(nangka);
+
+                //kondisi panjang nya kosong
+                if (aAngka >= 11) {
+                    //memberikan warning berupa eror
+                    txthasil.setText(" Bigger Than 10");
+                } else if (aAngka == 10) {
+                    //Kondisi ketika lebarnya kosong
+                    txthasil.setText("True");
+                } else if (aAngka <= 10) {
+                    txthasil.setText("Smaller Than 10");
+                }
+
+
+            }
+        });
+    }
+}
